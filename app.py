302da3548a136f0c3b886549d44884bd4464238f
Divyesh <divcodings@gmail.com>
2025-06-07 14:40:26 +0530

    Use timezone-aware datetime.now(timezone.utc) for date_created default

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
@@ -10,7 +10,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
 @app.route("/", methods=['POST', 'GET'])
 def this():
